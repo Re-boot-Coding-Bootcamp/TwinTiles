218a531357fc70faa4bcedff3750735ed5a9ec5e
Muradil Erkin <miller.muradil@gmail.com>
2024-02-14 20:16:25 -0500

    timer completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,33 @@
-import { AppBar, Button, IconButton, Toolbar } from "@mui/material";
+import { AppBar, Box, Button, IconButton, Toolbar } from "@mui/material";
 import React, { useContext } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import { orange } from "@mui/material/colors";
 import { CardDataContext } from "../context/CardDataContext";
+import { Timer } from "./Timer";
 
 const Navbar = () => {
   const { handleNewGame } = useContext(CardDataContext);
 
   return (
     <AppBar position="static" sx={{ bgcolor: orange[300] }}>
-      <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Button color="inherit" sx={{ mx: 1 }} onClick={handleNewGame}>
-          New Game
-        </Button>
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Box id="navbar-left-side">
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Button color="inherit" sx={{ mx: 1 }} onClick={handleNewGame}>
+            New Game
+          </Button>
+        </Box>
+        <Box id="navbar-right-side">
+          <Timer />
+        </Box>
       </Toolbar>
     </AppBar>
   );
